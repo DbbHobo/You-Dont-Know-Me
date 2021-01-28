16c392c07b606b95d758b732a302ec45a02c8828
dbbhobo <dmmnoodles@163.com>
2021-01-28 14:24:39 +0800

    date

---
+++ JavaScript/Data Structures /Date/Date.js
@@ -0,0 +1,8 @@
+let nowDate = new Date();
+console.log('getFullYear', nowDate.getFullYear());
+console.log('getMonth', nowDate.getMonth());
+console.log('getDate', nowDate.getDate());
+console.log('getHours', nowDate.getHours());
+console.log('getMinutes', nowDate.getMinutes());
+console.log('getSeconds', nowDate.getSeconds());
+console.log('getTime', nowDate.getTime());
